6559205e0d6664d51cc30fb47d6ee272199a6fe7
Kelvin Jardin <kelvin.r.jardin@gmail.com>
2025-06-01 10:45:05 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.npm_package_name ?? '')
+    .setDescription(process.env.npm_package_description ?? '')
+    .setVersion(process.env.npm_package_version ?? '')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   // Listen on all network interfaces
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
