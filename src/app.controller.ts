2dbe605120c2cd3ed7dc26d05a8b22912f514a57
Kelvin Jardin <kelvin.jardin@dataplan.co.uk>
2025-06-01 10:33:47 +0100

    main - corrected dockerfile not reloading content

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get(':id')
+  hello(@Param('id') id: string): string {
+    return this.appService.getHello();
+  }
+
   @Get()
-  getHello(): string {
+  helloAll(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  createHello(): string {
     return this.appService.getHello();
   }
 }
