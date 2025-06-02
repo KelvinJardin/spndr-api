f6018a1ba7cd5859fb8290ab48e5808da40697f3
Kelvin Jardin <kelvin.r.jardin@gmail.com>
2025-06-02 23:54:59 +0100

    Update Prisma schema and seed files

--- prisma/seeds/taxYears.ts
+++ prisma/seeds/taxYears.ts
@@ -3,15 +3,19 @@ import { PrismaClient } from '@prisma/client';
 export async function seedTaxYears(prisma: PrismaClient) {
   const taxYears = [
     {
+      startYear: 2024,
       startDate: new Date('2024-04-06'),
       endDate: new Date('2025-04-05'),
       isCurrent: true,
     },
     {
+      startYear: 2023,
       startDate: new Date('2023-04-06'),
       endDate: new Date('2024-04-05'),
+      isCurrent: false,
     },
     {
+      startYear: 2022,
       startDate: new Date('2022-04-06'),
       endDate: new Date('2023-04-05'),
       isCurrent: false,
