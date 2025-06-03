507723ac4be60344df25095cb3a5525a98662870
Kelvin Jardin <9026535+KelvinJardin@users.noreply.github.com>
2025-06-03 22:37:59 +0100

    main - bruno and tax box migration

---
+++ prisma/wipe.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const tables: string[] = [
+  'transaction',
+  'hobby',
+  'authenticator',
+  'session',
+  'verificationToken',
+  'user',
+  'transactionCategory',
+  'taxYear',
+]
+
+async function main() {
+  console.log('🧹 Cleaning up existing data...');
+
+  for (const table of tables) {
+    await prisma[table].deleteMany();
+  }
+
+  console.log('🧹 All existing data deleted!');
+}
+
+main()
+  .catch((e) => {
+    console.error('Error wiping database:', e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
