a36c4b726d3dd076fc22f3a533876e9f97d95d29
Kelvin Jardin <kelvin.r.jardin@gmail.com>
2025-06-03 02:29:31 +0100

    Fix StatsService dependency injection

--- src/modules/hobbies/hobbies.module.ts
+++ src/modules/hobbies/hobbies.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HobbiesController } from './hobbies.controller';
 import { HobbiesService } from './hobbies.service';
+import { StatsService } from '../../services/stats.service';
 
 @Module({
   controllers: [HobbiesController],
-  providers: [HobbiesService],
+  providers: [HobbiesService, StatsService],
   exports: [HobbiesService],
 })
 export class HobbiesModule {}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { StatsService } from '../../services/stats.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, StatsService],
   exports: [UsersService],
 })
 export class UsersModule {}
